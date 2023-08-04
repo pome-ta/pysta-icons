5f2c1674f2edbeaab4450e405ae286f19214f913
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-08-05 02:18:25 +0900

    up

---
+++ sandbox/230805_0206.py
@@ -0,0 +1,18 @@
+from objc_util import ObjCClass, nsurl
+#from objc_util import NSDictionary
+
+import pdbg
+
+NSDictionary = ObjCClass('NSDictionary')
+
+symbol_search_plist = '/System/Library/CoreServices/CoreGlyphs.bundle/symbol_search.plist'
+
+#plist_dic = NSDictionary.new().initWithContentsOfFile_(symbol_search_plist)
+
+#plist_dic = NSDictionary.alloc().initWithContentsOfURL_(nsurl(symbol_search_plist))
+
+plist_dic = NSDictionary.new().initWithContentsOfURL_(
+  nsurl(symbol_search_plist))
+
+#pdbg.state(NSDictionary.new())
+
