e5286ca26a0c433b67fa063c15c6375a4629b5a0
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-08-05 16:07:47 +0900

    up

--- sandbox/230805_1552.py
+++ sandbox/230805_1552.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 import plistlib
 
@@ -7,5 +8,20 @@ _path = '/System/Library/CoreServices/CoreGlyphs.bundle/'
 
 coreGlyphsBundle_path = Path(_path)
 
-al = list(coreGlyphsBundle_path.glob('*.plist'))
+plist_gene = coreGlyphsBundle_path.glob('*.plist')
+'''
+plist_list = [{
+  f'{pl.name}': plistlib.loads(pl.read_bytes())
+} for pl in plist_gene]
+'''
+
+plist_list_dics = [plistlib.loads(pl.read_bytes()) for pl in plist_gene]
+
+#[print(len(i)) for i in plist_list_dics]
+
+test_dic = plist_list_dics[5]
+#print(len(test_dic))
+test_data = json.dumps(test_dic, ensure_ascii=False, indent=2)
+
+print(test_data)
 
