e0f53a0fcc432b952c3cbdf737f8499fb19195e6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-08-05 14:04:53 +0900

    up

--- sandbox/230805_1317.py
+++ sandbox/230805_1317.py
@@ -5,5 +5,5 @@ coreGlyphsBundle_path = '/System/Library/CoreServices/CoreGlyphs.bundle/symbol_s
 
 path = Path(coreGlyphsBundle_path)
 
-a = path.read_bytes()
-aa = plistlib.loads(a)
+symbol_search = path.read_bytes()
+aa = plistlib.loads(symbol_search)
