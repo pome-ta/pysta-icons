f03c1cfd81552e00f34ff645a47c397bf4002265
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-08-05 13:29:17 +0900

    up

--- sandbox/230805_1317.py
+++ sandbox/230805_1317.py
@@ -1,28 +1,9 @@
-from objc_util import ObjCClass, nsurl
-#from objc_util import NSDictionary
+from pathlib import Path
+import plistlib
 
-import pdbg
+coreGlyphsBundle_path = '/System/Library/CoreServices/CoreGlyphs.bundle/symbol_search.plist'
 
-NSBundle = ObjCClass('NSBundle')
-#NSDictionary = ObjCClass('NSDictionary')
+path = Path(coreGlyphsBundle_path)
 
-symbol_search_plist = '/System/Library/CoreServices/CoreGlyphs.bundle/symbol_search.plist'
-
-#path = NSBundle.bundleWithPath_(symbol_search_plist).bundlePath()
-
-#plist_dic = NSDictionary.new().initWithContentsOfFile_(symbol_search_plist)
-
-#plist_dic = NSDictionary.alloc().initWithContentsOfURL_(nsurl(symbol_search_plist))
-
-#plist_dic = NSDictionary.new().initWithContentsOfURL_(nsurl(symbol_search_plist))
-
-#pdbg.state(NSDictionary.new())
-
-#pdbg.state(NSBundle.mainBundle())
-
-coreGlyphsBundle_path = '/System/Library/CoreServices/CoreGlyphs.bundle'
-
-bundle = NSBundle.bundleWithPath_(coreGlyphsBundle_path).bundlePath()
-
-pdbg.state(bundle)
-
+a = path.read_bytes()
+aa = plistlib.loads(a)
