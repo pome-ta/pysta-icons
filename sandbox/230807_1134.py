9f2094fce9a3b48c6c2ae6770ca2c22dc17fb8b6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-08-07 17:02:20 +0900

    up

--- sandbox/230807_1134.py
+++ sandbox/230807_1134.py
@@ -5,13 +5,18 @@ import ctypes
 from objc_util import ObjCClass, ObjCInstance, c
 import ui
 
-UIImage = ObjCClass('UIImage')
-UIImageSymbolConfiguration = ObjCClass('UIImageSymbolConfiguration')
-
 
 def get_UIImagePNGRepresentation():
   _UIImagePNGRepresentation = c.UIImagePNGRepresentation
+  _UIImagePNGRepresentation.argtypes = [ctypes.c_void_p]
+  _UIImagePNGRepresentation.restype = ctypes.c_void_p
+  return _UIImagePNGRepresentation
 
+
+UIImage = ObjCClass('UIImage')
+UIImageSymbolConfiguration = ObjCClass('UIImageSymbolConfiguration')
+
+UIImagePNGRepresentation = get_UIImagePNGRepresentation()
 
 json_path = Path('./dumps/symbol_order.plist.json')
 json_text = json_path.read_text()
@@ -27,7 +32,4 @@ class View(ui.View):
   def __init__(self, *args, **kwargs):
     super().__init__(self, *args, **kwargs)
     pass
-
-
-a = ui.Image
 
